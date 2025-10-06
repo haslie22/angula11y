9012a877748d3629f558884d3a636b4bdc6cca87
Lizaveta <lizadondysh@icloud.com>
2025-10-06 19:46:44 +0300

    feat: add Home component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 
+import { Home } from './home/home';
+
 @Component({
   selector: 'app-root',
-  imports: [],
-  template: ` <h1>Default</h1> `,
+  imports: [Home],
+  template: `
+    <main>
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
+  `,
   styleUrls: ['./app.css'],
 })
-export class App {
-  title = 'default';
-}
+export class App {}
