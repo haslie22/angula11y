cecd0153d05469714a466b02fab80f3f0f93a719
Lizaveta <lizadondysh@icloud.com>
2025-10-07 16:11:53 +0300

    refactor: combine several structural and code cleanups
    - removed unused field in App component
    - removed unused property in HousingLocation component
    - moved <header> tag outside <main> to improve accessibility
    - moved search form to a separate component
    - extracted inline templates into separate HTML files
    - added access modifiers to class fields

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,17 +5,7 @@ import { Home } from './home/home';
 @Component({
   selector: 'app-root',
   imports: [Home],
-  template: `
-    <main>
-      <header class="brand-name">
-        <img class="brand-logo" src="/assets/logo.svg" alt="" />
-        <h1 class="sr-only">Explore Rental Houses at the Best Price</h1>
-      </header>
-      <section class="content">
-        <app-home></app-home>
-      </section>
-    </main>
-  `,
+  templateUrl: './app.html',
   styleUrls: ['./app.css'],
 })
 export class App {}
--- src/app/housing-location/housing-location.ts
+++ src/app/housing-location/housing-location.ts
@@ -4,19 +4,7 @@ import { HousingLocationInfo } from '../housing-location.interface';
 
 @Component({
   selector: 'app-housing-location',
-  imports: [],
-  template: `
-    <section class="listing">
-      <img
-        class="listing-photo"
-        [src]="housingLocation().photo"
-        alt="Exterior photo of {{ housingLocation().name }}"
-        crossorigin
-      />
-      <h3 class="listing-heading">{{ housingLocation().name }}</h3>
-      <p class="listing-location">{{ housingLocation().city }}, {{ housingLocation().state }}</p>
-    </section>
-  `,
+  templateUrl: './housing-location.html',
   styleUrls: ['./housing-location.css'],
 })
 export class HousingLocation {
